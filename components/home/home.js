e6637bfd8bd55bf4e5379f2db265431922f7d4d5
wenxiaofang <1593744037@qq.com>
2017-04-20 23:13:15 +0800

    '文小芳第二次提交，轮播图+首页到热点资讯部分'

--- components/home/home.js
+++ components/home/home.js
@@ -6,9 +6,25 @@ angular.module('homeModule',[])
             templateUrl: 'components/home/home.html',
             controller:'homeCtrl',
             css:'components/home/home.css'
+//          css:['components/home/home.css','css/swiper-3.4.1.min.css']
         })
 })
-.controller('homeCtrl',['$scope',function($scope){
-	$scope.name = '熊1';
-	$scope.show='我是首页'
+
+.service('swiper',['$timeout',function($timeout){
+	$timeout(function(){
+		new Swiper ('.swiper-container', {
+		    loop: true,
+		    autoplay:2000,
+		    // 如果需要分页器
+		    pagination: '.swiper-pagination',
+		  }) 
+	},10);
+}])
+
+
+.controller('homeCtrl',['$scope','swiper','$http',function($scope,$http){
+	$http.get('components/home/json/view.json').success(function(res){
+		$scope.data=res.msg;
+		console.log($scope.data);
+	})
 }])
